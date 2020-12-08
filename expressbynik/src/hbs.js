ef98212e692551d90015d59d86c57500f2a27c34
Nikumani Choudhury <nikunchoudhury@gmail.com>
2020-12-08 18:58:28 +0530

    Express JS Project

--- expressbynik/src/hbs.js
+++ expressbynik/src/hbs.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = 8080;
+const hbs = require('hbs');
 
 
 // Geting Path of public Folder
@@ -10,7 +11,8 @@ const port = 8080;
 // console.log(path.join(__dirname,"../public"))
 
 const staticPath = path.join(__dirname,"../public");
-const templatePath = path.join(__dirname,"../templates");
+const templatePath = path.join(__dirname,"../templates/views");
+const partialPath = path.join(__dirname,"../templates/partials");
 
 
 
@@ -18,29 +20,42 @@ const templatePath = path.join(__dirname,"../templates");
 // Using hbs as the default view engine requires just one line of code in your app setup. This will render .hbs files when res.render is called.
 app.set("view engine", "hbs");
 app.set("views", templatePath);
+hbs.registerPartials(partialPath);
+
+app.use(express.static(staticPath));
+
+// app.get(route, callback)
 
 
 app.get("/", (req, res)=>{
     res.render("index",{
-        myName: "Niku" //This is an Object
+        myName: "Niku Mani" //This is an Object
     });
    
 });
 
 app.get("/about", (req, res)=>{
-    res.render("about");
+    res.render("about",{
+        myName: "Niku Mani" //This is an Object
+    });
    
 });
 
 
+app.get("/about/*",(req,res)=>{    //Unversal-oparator "*"
+    res.render("404",{
+        myName: "Niku Mani",
+        errorcomment:"Opps...This about Page is Not Found !!"
+    });
+})
 
-// app.get(route, callback)
-app.get("/",(req, res)=>{
-    res.send("<h1>Hello Form the Express Side !!! </h1>");
-});
-app.get("/about",(req, res)=>{
-    res.send("<h1>Hello Form the about Side !!! </h1>");
-});
+
+app.get("*",(req,res)=>{    //Unversal-oparator "*"
+    res.render("404",{
+        myName: "Niku Mani",
+        errorcomment:"Opps... This Page is Not Found !!"
+    });
+})
 
 
 
